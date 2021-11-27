c19727a4a37b5b1ec9c9e35cc4c7cd97838fa71a
Paul Reichelt <paul.reichelt@outlook.com>
2021-11-27 11:58:24 +0100

    Init empty mod

---
+++ src/main/java/com/minimammoth/ironoak/IronOak.java
@@ -0,0 +1,10 @@
+package com.minimammoth.ironoak;
+
+import net.fabricmc.api.ModInitializer;
+
+public class IronOak implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/minimammoth/ironoak/client/IronOakClient.java
@@ -0,0 +1,11 @@
+package com.minimammoth.ironoak.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class IronOakClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
