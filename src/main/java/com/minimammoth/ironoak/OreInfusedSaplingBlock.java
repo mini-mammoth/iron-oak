ba2f25dbc4e1e4814bd6c9e584bfc1ca54dfea63
Paul Reichelt <paul.reichelt@outlook.com>
2022-06-22 21:57:46 +0200

    deps: Upgrade to 1.18.2
    Migrate to new TreeFeature API

--- src/main/java/com/minimammoth/ironoak/OreInfusedSaplingBlock.java
+++ src/main/java/com/minimammoth/ironoak/OreInfusedSaplingBlock.java
@@ -2,18 +2,17 @@ package com.minimammoth.ironoak;
 
 import net.minecraft.block.SaplingBlock;
 import net.minecraft.block.sapling.SaplingGenerator;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 import java.util.function.Supplier;
 
 public class OreInfusedSaplingBlock extends SaplingBlock {
-    public OreInfusedSaplingBlock(final Supplier<ConfiguredFeature<?, ?>> treeFeature, Settings settings) {
+    public OreInfusedSaplingBlock(final Supplier<RegistryEntry<? extends ConfiguredFeature<?, ?>>> treeFeature, Settings settings) {
         super(new SaplingGenerator() {
-            @Nullable
             @Override
-            protected ConfiguredFeature<?, ?> getTreeFeature(Random random, boolean bees) {
+            protected RegistryEntry<? extends ConfiguredFeature<?, ?>> getTreeFeature(Random random, boolean bees) {
                 return treeFeature.get();
             }
         }, settings);
--- src/main/java/com/minimammoth/ironoak/init/ModConfiguredFeatures.java
+++ src/main/java/com/minimammoth/ironoak/init/ModConfiguredFeatures.java
@@ -1,11 +1,14 @@
 package com.minimammoth.ironoak.init;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.ConfiguredFeatures;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
@@ -19,39 +22,23 @@ public class ModConfiguredFeatures {
     private ModConfiguredFeatures() {
     }
 
-    public static final ConfiguredFeature<?, ?> COPPER_OAK_TREE = Feature.TREE
-            // Configure the feature using the builder
-            .configure(new TreeFeatureConfig.Builder(
-                    BlockStateProvider.of(ModBlocks.COPPER_OAK_LOG), // Trunk block provider
-                    new StraightTrunkPlacer(4, 2, 0), // places a straight trunk
-                    BlockStateProvider.of(Blocks.OAK_LEAVES), // Foliage block provider
-                    new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 3), // places leaves as a blob (radius, offset from trunk, height)
-                    new TwoLayersFeatureSize(1, 0, 1) // The width of the tree at different layers; used to see how tall the tree can be without clipping into blocks
-            ).ignoreVines().build());
+    public static RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>> COPPER_OAK_TREE;
+    public static RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>> GOLD_OAK_TREE;
+    public static RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>> IRON_OAK_TREE;
 
-    public static final ConfiguredFeature<?, ?> GOLD_OAK_TREE = Feature.TREE
-            // Configure the feature using the builder
-            .configure(new TreeFeatureConfig.Builder(
-                    BlockStateProvider.of(ModBlocks.GOLD_OAK_LOG), // Trunk block provider
-                    new StraightTrunkPlacer(4, 2, 0), // places a straight trunk
-                    BlockStateProvider.of(Blocks.OAK_LEAVES), // Foliage block provider
-                    new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 3), // places leaves as a blob (radius, offset from trunk, height)
-                    new TwoLayersFeatureSize(1, 0, 1) // The width of the tree at different layers; used to see how tall the tree can be without clipping into blocks
-            ).ignoreVines().build());
-
-    public static final ConfiguredFeature<?, ?> IRON_OAK_TREE = Feature.TREE
-            // Configure the feature using the builder
-            .configure(new TreeFeatureConfig.Builder(
-                    BlockStateProvider.of(ModBlocks.IRON_OAK_LOG), // Trunk block provider
-                    new StraightTrunkPlacer(4, 2, 0), // places a straight trunk
-                    BlockStateProvider.of(Blocks.OAK_LEAVES), // Foliage block provider
-                    new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 3), // places leaves as a blob (radius, offset from trunk, height)
-                    new TwoLayersFeatureSize(1, 0, 1) // The width of the tree at different layers; used to see how tall the tree can be without clipping into blocks
-            ).ignoreVines().build());
+    private static TreeFeatureConfig.Builder oreOak(Block copperOakLog) {
+        return new TreeFeatureConfig.Builder(
+                BlockStateProvider.of(copperOakLog), // Trunk block provider
+                new StraightTrunkPlacer(4, 2, 0), // places a straight trunk
+                BlockStateProvider.of(Blocks.OAK_LEAVES), // Foliage block provider
+                new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 3), // places leaves as a blob (radius, offset from trunk, height)
+                new TwoLayersFeatureSize(1, 0, 1) // The width of the tree at different layers; used to see how tall the tree can be without clipping into blocks
+        ).ignoreVines();
+    }
 
     public static void onInitialize() {
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(MOD_ID, "copper_oak_tree"), COPPER_OAK_TREE);
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(MOD_ID, "gold_oak_tree"), GOLD_OAK_TREE);
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(MOD_ID, "iron_oak_tree"), IRON_OAK_TREE);
+        COPPER_OAK_TREE = ConfiguredFeatures.register(new Identifier(MOD_ID, "copper_oak_tree").toString(), Feature.TREE, oreOak(ModBlocks.COPPER_OAK_LOG).build());
+        GOLD_OAK_TREE = ConfiguredFeatures.register(new Identifier(MOD_ID, "gold_oak_tree").toString(), Feature.TREE, oreOak(ModBlocks.GOLD_OAK_LOG).build());
+        IRON_OAK_TREE = ConfiguredFeatures.register(new Identifier(MOD_ID, "iron_oak_tree").toString(), Feature.TREE, oreOak(ModBlocks.IRON_OAK_LOG).build());
     }
 }
